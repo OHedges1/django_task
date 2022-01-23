cd0665bbad5c1c0ffd52f93eb5a2dbf9b4bb5bb7
Oliver Hedges <oliver@drystone.co.uk>
2022-01-23 15:16:45 +0000

    added retrieve_from_db

--- app/holidays/views.py
+++ app/holidays/views.py
@@ -6,23 +6,28 @@ import requests
 def index(request):
     return HttpResponse("hello, world. You're at the index.")
 
-def retrive_from_db():
-    ...
+def retrieve_from_db(request, countryCode_str):
+    entrys = Holidays.objects.filter(countryCode__countryCode = countryCode_str)
+    country_name = CountryCodes.objects.filter(pk = countryCode_str)
+    if not country_name:
+        return HttpResponse('Country code not found')
+    data = {'holidays': entrys, 'country_name': country_name[0].countryName}#country_name[0].countryName}
+    return render(request, 'table_template.html', data)
+
 
 def fetch_and_save(request):
     holidayFields = [f.name for f in Holidays._meta.get_fields()][1:] # slice to remove 'id'
     objs = CountryCodes.objects.all()
-
     for obj in objs:
         key = obj.pk
         responses = requests.get('https://date.nager.at/api/v3/NextPublicHolidays/{}'.format(key)).json()
-
+        print('saving {}: holidays to database'.format(key))
         for response in responses:
             tmp = {key: response[key] for key in holidayFields}
             tmp['countryCode'] = obj
-            newObj = Holidays(**tmp)
+            newObj = Holidays(**tmp) # look at setattr()
             newObj.save()
-
+    print('saving successful')
 
 
     return HttpResponse('saved')
